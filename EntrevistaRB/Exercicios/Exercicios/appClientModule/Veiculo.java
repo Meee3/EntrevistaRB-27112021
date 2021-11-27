67181f5fcaa2056c82b1d3064f07d1afce0ad8ca
Unnamed <51212563+Meee3@users.noreply.github.com>
2021-11-27 15:45:22 -0300

    Finalizando

---
+++ EntrevistaRB/Exercicios/Exercicios/appClientModule/Veiculo.java
@@ -0,0 +1,25 @@
+
+public class Veiculo {
+	
+	protected short velocidade;
+	
+	
+	
+	Veiculo(){}
+	
+	public Veiculo(short velocidade) {
+	 this.velocidade = velocidade;
+	}
+
+	public short getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(short velocidade) {
+		this.velocidade = velocidade;
+	}
+	
+	
+	
+
+}
